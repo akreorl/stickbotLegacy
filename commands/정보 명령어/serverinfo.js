812bdb2ce5168e6386afa0dd94118ac1e4202470
jeongteak06 <60865072+jeongteak06@users.noreply.github.com>
2020-08-11 16:54:43 +0900

    Update serverinfo.js

--- commands/정보 명령어/serverinfo.js
+++ commands/정보 명령어/serverinfo.js
@@ -1,7 +1,15 @@
 const { MessageEmbed, DiscordAPIError, Discord } = require("discord.js")
 
+const verificationLevels = {
+	NONE: '아무것도 보호하지 않아도되요',
+	LOW: '보안이 낮아요(이메일이 인증되야해요)',
+	MEDIUM: '보안이 중간이에요(5분이 지나야 서버를 이용하실수있어요)',
+	HIGH: '보안이 높아요(10분이 지나야 서버를 이용하실수있어요)',
+	VERY_HIGH: '보안이 매우 높아요(휴대폰인증이 완료되야해요)'
+};
+
 module.exports = {
-    name: "서버정보",
+    name: "테스트서버정보",
     category: "정보 명령어",
     description: "서버 정보를 출력합니다",
     run: async (bot, message, args) => {
@@ -19,8 +27,13 @@ module.exports = {
             .addField("멤버 수", `<:disocrd:742021671497629737> ${message.guild.memberCount}`, inline)
             .addField("역할 개수", message.guild.roles.cache.size, inline)
             .addField("채널 개수", message.guild.channels.cache.size, inline)
-            .addField("가입하신 날", message.member.joinedAt)
-            .setFooter(`생일:${message.guild.createdAt}`);
+            .addField('서버 부스트', [`${message.guild.premiumTier ? `<a:nitro:742586121908781136>level ${message.guild.premiumTier}` : '<a:no:742569138764906628>아무도 부스트 하지 않음'}`], inline)
+            .addField('부스트 수',` <a:nitro:742586121908781136> ${message.guild.premiumSubscriptionCount || '0'}`, inline)
+            .addField("서버 보안 레벨",`${verificationLevels[message.guild.verificationLevel]}`, false)
+            .addField("서버 가입일", message.member.joinedAt)
+            .addField("생일", `${message.guild.createdAt}`)
+            .setTimestamp()
+            .setFooter(`${message.guild.name} 의 정보`);
         
             message.channel.send(serverembed);
     }
