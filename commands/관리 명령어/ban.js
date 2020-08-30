0bbc7a21c65adbe28c41b8672a12c2ce81f76173
jeongteak06 <60865072+jeongteak06@users.noreply.github.com>
2020-08-30 23:15:04 +0900

    Update ban.js

--- commands/관리 명령어/ban.js
+++ commands/관리 명령어/ban.js
@@ -1,4 +1,4 @@
-const  MessageEmbed  = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 const { promptMessage } = require("../../functions.js");
 
@@ -12,39 +12,39 @@ module.exports = {
 
         if (!args[0]) {
             return message.reply("<a:no:742569138764906628>밴할 사람을 멘션해 주세요")
-                .then(m => m.delete(5000));
+                
         }
 
         if (!args[1]) {
             return message.reply("<a:no:742569138764906628>밴 하는 이유를 적어주세요")
-                .then(m => m.delete(5000));
+                
         }
 
         if (!message.member.hasPermission("BAN_MEMBERS")) {
-            return message.reply("<a:no:742569138764906628> 당신은 유저 밴 권한이 없습니다")
-                .then(m => m.delete(5000));
+            return message.reply("<a:no:742569138764906628> 당신은 유저 차단 권한이 없습니다")
+                
         
         }
         if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
-            return message.reply("<a:no:742569138764906628> 막대기봇이 밴 권한을 가지고 있지 않습니다")
-                .then(m => m.delete(5000));
+            return message.reply("<a:no:742569138764906628> 막대기봇이 차단 권한을 가지고 있지 않습니다")
+                
         }
 
         const toBan = message.mentions.members.first() || message.guild.members.get(args[0]);
 
         if (!toBan) {
             return message.reply("<a:no:742569138764906628>해당 멤버를 찾을수 없습니다, 다시 시도해 주세요")
-                .then(m => m.delete(5000));
+                
         }
 
         if (toBan.id === message.author.id) {
             return message.reply("<a:no:742569138764906628>자기 자신을 밴 할수 없습니다")
-                .then(m => m.delete(5000));
+                
         }
 
         if (!toBan.bannable) {
-            return message.reply("<a:no:742569138764906628>역할 순위가 높아 밴 할수 없습니다")
-                .then(m => m.delete(5000));
+            return message.reply("<a:no:742569138764906628>역할 순위가 높아 차단할수 없습니다")
+                
         }
         
         const embed = new MessageEmbed()
@@ -59,23 +59,22 @@ module.exports = {
         const promptEmbed = new MessageEmbed()
             .setColor("GREEN")
             .setAuthor(`30초가 지나면 무효가 됩니다`)
-            .setDescription(` ${toBan} 을 밴하시겟습니까?`)
+            .setDescription(`${toBan}을 밴하시겟습니까??`)
 
         await message.channel.send(promptEmbed).then(async msg => {
             const emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
 
             if (emoji === "✅") {
-                  message.reply(`<a:yes:742568893351985303>밴 완료됨.`)
+                message.reply(`<a:yes:742568893351985303> 밴 완료됨`)
 
                 toBan.ban(args.slice(1).join(" "))
                     .catch(err => {
-                        if (err) return message.channel.send(`밴이 되지 않습니다... 오류 발생 ${err}`)
+                        if (err) return message.channel.send(`밴이 되지 않습니다...오류 발생 ${err}`)
                     });
 
-                logChannel.send(embed);
-            } else if (emoji === "❌") {
-
-                message.reply(`<a:no:742569138764906628>밴 취소됨.`)
+                } else if (emoji === "❌") {
+    
+                    message.reply(`<a:no:742569138764906628> 밴 취소됨`)
             }
         });
     }
