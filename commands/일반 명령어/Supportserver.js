cdc4d91110de375164af5346e51ad297e82dd74c
jeongteak06 <60865072+jeongteak06@users.noreply.github.com>
2020-08-30 21:43:14 +0900

    Update Supportserver.js

--- commands/일반 명령어/Supportserver.js
+++ commands/일반 명령어/Supportserver.js
@@ -3,6 +3,6 @@ module.exports = {
     category: "일반 명령어",
     description: "막대기봇 서포트 서버 링크",
     run: async (client, message, args) => {
-        const msg = await message.channel.send('https://discord.gg/VYt9MT \n 막대기봇 서포트 서버에요! \n 가입하셔서 문의사항이나 봇이 더 발전할수 있도록 여려분의 의견을 부탁해요');
+        const msg = await message.channel.send('https://discord.gg/kV8Wezw \n 막대기봇 서포트 서버에요! \n 가입하셔서 문의사항이나 봇이 더 발전할수 있도록 여려분의 의견을 부탁해요');
     }
 }
