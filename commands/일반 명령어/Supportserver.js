de45453b09c8c57b2f0ed3dac9abf814f79ad39c
Stick <jeongteak06@naver.com>
2020-09-08 10:22:05 +0900

    Update Supportserver.js

--- commands/일반 명령어/Supportserver.js
+++ commands/일반 명령어/Supportserver.js
@@ -3,6 +3,6 @@ module.exports = {
     category: "일반 명령어",
     description: "막대기봇 서포트 서버 링크",
     run: async (client, message, args) => {
-        const msg = await message.channel.send('https://discord.gg/kV8Wezw \n 막대기봇 서포트 서버 ***(엿던 것)*** 에요! \n 가입하셔서 문의사항이나 봇이 더 발전할수 있도록 여려분의 의견을 부탁해요 \n 병맛 서버의 병맛 초대사이트: https://jeongteak06.github.io/serverinvite/ ');
+        const msg = await message.channel.send('https://discord.gg/kV8Wezw \n 막대기봇 서포트 서버 ***(엿던 것)*** 에요! \n 가입하셔서 문의사항이나 봇이 더 발전할수 있도록 여려분의 의견을 부탁해요 \n 병맛 서버의 병맛 초대사이트: https://jeongtaek06.github.io/serverinvite/ ');
     }
 }
